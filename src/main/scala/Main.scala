48177b1d01a4318b20da831a9a201c1a80147d24
Phlilipp <philippbredow@gmail.com>
2021-06-08 16:27:43 +0200

    Updated Design to new Requirements Part 2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,32 +10,33 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{RequestContext, RouteResult}
 import com.mongodb.client.model.Filters
-import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
+import com.mongodb.spark.MongoSpark
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.bson.Document
+import org.json4s.DefaultFormats
+import org.json4s.jackson.Json
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.io.StdIn
 
 object Main extends CORSHandler {
 
-  val partyMatcher = Map("cdu" -> "CDU", "spd" -> "SPD", "fdp" -> "FDP", "linke" -> "Linke", "gruene" -> "B90", "afd" -> "AfD", "csu" -> "CSU", "parteilos" -> "Parteilos")
+  val partyMatcher = Map("CDU" -> "CDU", "SPD" -> "SPD", "FDP" -> "FDP", "Linke" -> "Linke", "B90" -> "B90", "AfD" -> "AfD", "CSU" -> "CSU", "Parteilos" -> "Parteilos")
 
   def main(args: Array[String]): Unit = {
-    val mongoClient: MongoClient = MongoClient(sys.env("REAGENT_MONGO") + "?authSource=examples")
-    val database: MongoDatabase = mongoClient.getDatabase("examples")
-
-    val collectionProcessed: MongoCollection[Document] = database.getCollection("ProcessedTweets")
-    val collectionOriginal: MongoCollection[Document] = database.getCollection("tweets_bundestag_complete")
-
-    val collectionCountTotalByYear: MongoCollection[Document] = database.getCollection("countTotalByYear")
 
     implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "my-system")
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
 
+    val countTotalRDD = getRDD("countTotalByYear")
+
+
     val routes = {
       concat(
-        getRoutesWithPartyYearMonth("countTweetByMonth", collectionCountTotalByYear, collectionCountTotalByYear)
+        getRoutesWithPartyYearMonth("countTweetByMonth", countTotalRDD, countTotalRDD, countTotalRDD)
       )
     }
 
@@ -50,24 +51,49 @@ object Main extends CORSHandler {
       .onComplete(_ => system.terminate()) // and shutdown when done
   }
 
+
+  /**
+   * Liefert eine Mongo-Collection als RDD
+   * @param collectionName Name der Mongo-Collection
+   * @return RDD[Document]
+   */
+  def getRDD(collectionName: String): RDD[Document] = {
+    val sparkSession = SparkSession.builder()
+      .master("local")
+      .appName("MongoSparkConnector")
+      .config("spark.mongodb.input.uri", sys.env("REAGENT_MONGO") + "examples." + collectionName + "?authSource=examples")
+      .config("spark.testing.memory", 2147480000)
+      .getOrCreate()
+
+    val sc = sparkSession.sparkContext
+    MongoSpark.load(sc).rdd
+  }
+
+
   /**
    * Liefert Paths:
    * /path
    * /path/partei
    * /path/jahr
    * /path/jahr/partei
-   * /path/jahr/monat
-   * /path/jahr/monat/partei
+   * /path/jahr/"week" (time.getDayOfYear / 7) + 1
+   * /path/jahr/"month"
+   * /path/jahr/"week"/partei
+   * /path/jahr/"month"/partei
    */
-  def getRoutesWithPartyYearMonth(pathName: String, collectionYear: MongoCollection[Document], collectionMonth: MongoCollection[Document]): RequestContext => Future[RouteResult] = {
+  def getRoutesWithPartyYearMonth(pathName: String, collectionYear: RDD[Document], collectionMonth: RDD[Document], collectionWeek: RDD[Document]): RequestContext => Future[RouteResult] = {
     pathPrefix(pathName) {
       concat(
         pathEnd {
           get {
-            corsHandler(complete(HttpEntity(ContentTypes.`application/json`, collectionYear.find().results().map(_.toJson()).toArray.mkString("[", ",", "]"))))
+            corsHandler(complete(HttpEntity(ContentTypes.`application/json`, {
+              val temp = collectionYear.map(elem => elem.get("_id").asInstanceOf[Document].get("party") -> (elem.get("_id").asInstanceOf[Document].get("year").toString, elem.get("count")))
+                .groupBy(_._1).mapValues(_.map(_._2).toMap).collect()
+              Json(DefaultFormats).write(temp)
+            })))
           }
-        },
-        path(partyMatcher) { party =>
+        }
+        /*path(partyMatcher) { party =>
           get {
             corsHandler(complete(HttpEntity(ContentTypes.`application/json`, collectionYear.find(Filters.eq("_id.party", party)).results().map(_.toJson()).toArray.mkString("[", ",", "]"))))
           }
@@ -101,8 +127,39 @@ object Main extends CORSHandler {
               Filters.eq("_id.party", party))
             ).results().map(_.toJson()).toArray.mkString("[", ",", "]"))))
           }
-        }
+        }*/
       )
     }
   }
+
+  /*def main(args: Array[String]): Unit = {
+    val mongoClient: MongoClient = MongoClient(sys.env("REAGENT_MONGO") + "?authSource=examples")
+    val database: MongoDatabase = mongoClient.getDatabase("examples")
+
+    val collectionProcessed: MongoCollection[Document] = database.getCollection("ProcessedTweets")
+    val collectionOriginal: MongoCollection[Document] = database.getCollection("tweets_bundestag_complete")
+
+    val collectionCountTotalByYear: MongoCollection[Document] = database.getCollection("countTotalByYear")
+
+    implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "my-system")
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext: ExecutionContextExecutor = system.executionContext
+
+
+    val routes = {
+      concat(
+        getRoutesWithPartyYearMonth("countTweetByMonth", collectionCountTotalByYear, collectionCountTotalByYear, collectionCountTotalByYear)
+      )
+    }
+
+
+    val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(routes)
+
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }*/
 }
